8d63fe747be76c4623946000632ef8697a56092f
Joseph Irwin <joseph.irwin.gt@gmail.com>
2014-10-08 17:31:54 +0900

    Incorporate picojson library

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,30 @@
+#include <iostream>
+
+#include <experimental/optional>
+
+#include "picojson/picojson.h"
+
+
 int main() {
-    return 0;
+
+  picojson::value v;
+  std::cin >> v;
+  if (std::cin.fail()) {
+    std::cerr << picojson::get_last_error() << std::endl;
+    return 1;
+  }
+
+
+  size_t sentence_count = 0;
+  auto & sentences = v.get<picojson::object>()["sentences"].get<picojson::array>();
+  for (auto & s : sentences) {
+    ++sentence_count;
+    std::cout << "sentence "
+              << sentence_count
+              << " size: "
+              << s.get<picojson::object>()["words"].get<picojson::array>().size()
+              << std::endl;
+  }
+
+  return 0;
 }
