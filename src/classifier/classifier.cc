7d792eb0e75e9840621feb5d4f1a85301209f472
Joseph Irwin <joseph.irwin.gt@gmail.com>
2014-10-26 01:13:54 +0900

    Re-add incomplete code temporarily

--- src/classifier/classifier.cc
+++ src/classifier/classifier.cc
@@ -1,5 +1,72 @@
 #include "classifier.h"
 
+#include <iostream>
+#include <algorithm>
+
+#include <json11/json11.hpp>
+#include <foo/foo.h>
+
+using namespace foo;
+
+namespace {
+  //↓temporary code
+  json11::Json read_json(std::istream&) { return {}; }
+
+  void create_instances() {
+    auto v = read_json(std::cin);
+
+    size_t sentence_count = 0;
+    auto & sentences = v["sentences"].array_items();
+    for (auto & s : sentences) {
+      ++sentence_count;
+
+      std::vector<word_t> words;
+      words.reserve(s["words"].array_items().size());
+      std::transform(begin(s["words"].array_items()),
+                     end(s["words"].array_items()),
+                     std::back_inserter(words),
+                     [](auto js) { return js.string_value(); }
+                     );
+
+      std::vector<pos_t> tags;
+      tags.reserve(s["tags"].array_items().size());
+      std::transform(begin(s["tags"].array_items()),
+                     end(s["tags"].array_items()),
+                     std::back_inserter(tags),
+                     [](auto js) { return js.string_value(); }
+                     );
+
+      std::vector<span_t> spans;
+      auto const & jspans = s["parse"]["spans"].array_items();
+      spans.reserve(jspans.size());
+      std::transform(begin(jspans),
+                     end(jspans),
+                     std::back_inserter(spans),
+                     [](auto js) { return span_t((js[0].is_string() ? js[0].string_value() : "<t>"), js[1].int_value(), js[2].int_value()); });
+
+      std::cout << "sentence "
+                << sentence_count
+                << " num_words: "
+                << words.size()
+                << " num_tags: "
+                << tags.size()
+                << " num_spans: "
+                << spans.size()
+                << std::endl;
+
+      for (auto const &edge : s["parse"]["edges"].array_items()) {
+        auto const & head_span = spans[edge[0].int_value()];
+        std::cout << "Edge: " << head_span << " =>";
+        for (auto const &c : edge[1].array_items()) {
+          std::cout << " " << spans[c.int_value()];
+        }
+        std::cout << std::endl;
+      }
+    }
+  }
+}
+
+
 int classifier::train() { return 0; }
 int classifier::test() { return 0; }
 int classifier::run() { return 0; }
